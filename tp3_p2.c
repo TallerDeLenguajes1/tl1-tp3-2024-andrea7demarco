89dd6d9bc2bd6ba791567233e64c205ba0fadfa3
Andrea Sofia De Marco <andreasofiademarco@gmail.com>
2024-04-21 17:27:54 -0300

    punto terminado

--- tp3_p2.c
+++ tp3_p2.c
@@ -1,24 +1,30 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<time.h>
+#define F 5
+#define C 12
+
 
 int main()
 {
-  int M[5][12]; //fila año columna mes
-  int i,j;
+  int M[F][C]; //fila año columna mes
+  int i,j, Max = 0, Min = 99999;
+  float prom;
+  int anio_max  = 0, anio_min = 0, mes_max = 0 , mes_min = 0;
+  int gananciaTotal;
   srand(time(NULL));
   
-  for( i = 0; i  < 5; i++) {
-    for(j = 0; j < 12; j++)
+  for( i = 0; i  < F; i++) {
+    for(j = 0; j < C; j++)
     {
-    M[i][j]=(rand()%40001)+10000;
+    M[i][j]=rand()%40001+10000;
     }
   }
 
 
   printf("Matriz de valores\n");
-    for( i = 0; i  < 5; i++) {
-    for(j = 0; j < 12; j++)
+    for( i = 0; i  < F; i++) {
+    for(j = 0; j < C; j++)
     {
     printf( "%d ",M[i][j]);
 
@@ -27,5 +33,54 @@ int main()
     printf("\n");
 
   }
+
+  // promedio de ganancias por año 
+    for( i = 0; i  < F; i++) {
+    gananciaTotal = 0; 
+    for(j = 0; j < C; j++)
+    { 
+    gananciaTotal +=  M[i][j];
+
+    }
+    prom =  ((float)gananciaTotal/12);
+    printf("promedio ganancia del año %d : %.2f\n",i +1 ,prom );
+
+  }
+
+  // obtener max y minimo de ganancias 
+  //mostart año y mes que ocurrió
+   for( i = 0; i < F; i++ ) {
+
+    for (j = 0; j < C; j ++ )
+    {
+     if( M[i][j] > Max )
+     {
+      Max = M[i][j];
+      anio_max=i + 1;
+      mes_max=j + 1;
+     } else {
+      if( M[i][j] < Min)
+      {
+        Min = M[i][j];
+        mes_min = j + 1;
+        anio_min = i +1 ;
+        
+      }
+     }
+    }
+
+
+   }
+
+   
+   
+
+  printf("Max : %d, mes : %d, anio : %d\n",Max , mes_max, anio_max);
+  printf("Min : %d, mes : %d, anio : %d\n", Min, mes_min, anio_min);
+
+
+  
+
+  return 0;
 }
 
